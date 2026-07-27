80d96190536766082df61819e6c8f30a99dfcc08
Leonardo Jimenez <jimenezleonardop@gmail.com>
2026-07-26 23:40:09 -0500

    refactor: moar fixing

--- apps/mobile/lib/core/constants.dart
+++ apps/mobile/lib/core/constants.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://localhost:8000',
+    defaultValue: 'http://10.0.2.2:8000',
   );
 }
